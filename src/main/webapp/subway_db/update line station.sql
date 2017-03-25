21664b15b1efaa0110d0308eec63da686198a232
huangbin <876301469@qq.com>
2017-03-25 14:14:19 +0800

    c

--- src/main/webapp/subway_db/update line station.sql
+++ src/main/webapp/subway_db/update line station.sql
@@ -1,3 +1,6 @@
+
+
+set sql_safe_updates = 0;
 UPDATE t_locations l
         JOIN
     v_line ln ON LEFT(l.location, 4) = ln.line_no
@@ -5,4 +8,4 @@ UPDATE t_locations l
     v_station s ON LEFT(l.location, 6) = s.station_no 
 SET 
     l.line_id = ln.id,
-    l.station_id = s.id;
+    l.station_id = s.id
